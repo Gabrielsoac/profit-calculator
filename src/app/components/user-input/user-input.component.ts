2f98768a076ac23b0536e07380e3ad3035ac79c6
Gabriel Soares <gabrielsoacc@gmail.com>
2025-09-02 21:07:36 -0300

    feat: created two way biding to form investment data

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  investment = {
+    initialInvestment: '0',
+    annualInvestment: '0',
+    expectedReturn: '5',
+    duration: '10',
+  };
 
+  onSubmit() {
+    console.log(this.investment);
+  }
 }
