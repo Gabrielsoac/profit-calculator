6f856785d72a396ae73976cd1f9cf3ae37807a0c
Gabriel Soares <gabrielsoacc@gmail.com>
2025-09-06 22:34:29 -0300

    feat: added show results

--- src/app/components/investment-results/investment-results.component.ts
+++ src/app/components/investment-results/investment-results.component.ts
@@ -3,11 +3,12 @@ import {
   InvestmentData,
   InvestmentResult,
 } from '../../../interfaces/Investment';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
